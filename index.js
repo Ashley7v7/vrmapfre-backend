76da6b88ae17af051434fa57b220ccb2ac284aa0
Ashley7v7 <ashleymancillaflores@gmail.com>
2025-08-01 15:31:01 -0600

    Deploy backend

--- index.js
+++ index.js
@@ -110,10 +110,13 @@ app.post('/api/visitas-multiples', async (req, res) => {
   try {
     const { visitas } = req.body;
     console.log('📥 Visitas recibidas:', JSON.stringify(visitas, null, 2));
-    console.log('Creando visita con:', {
-      usoReporte: visita.usoReporte,
-      compartirCon: visita.compartirCon,
+    visitas.forEach(v => {
+      console.log('Creando visita con:', {
+        usoReporte: v.usoReporte,
+        compartirCon: v.compartirCon,
+      });
     });
+
 
     
 
